88b34eb65fa3c1e57497b0a8d4d194322f8b59fd
WebProject720 <rameshmalisundesha720@gmail.com>
2024-08-26 11:00:25 +0530

    login Api

--- src/Controllers/auth/login.js
+++ src/Controllers/auth/login.js
@@ -4,6 +4,7 @@ import { User } from '../../Models/models.js'
 import bcrypt from 'bcrypt'
 import { createToken } from "../../utils/getToken.js";
 import 'dotenv/config'
+import { Types } from "mongoose";
 import { getUser } from "../../components/getUser.js";
 
 
@@ -24,8 +25,13 @@ export const login = async (req, res) => {
                     new ApiError('All fileds required')
                 )
         }
+        const _id = identifier.length < 24 ? '66cb64e7ad5783c372a7f00c' : identifier
 
-        const user = await getUser(identifier)
+        const user = await User.findOne(
+            {
+                $or: [{ email: identifier }, { username: identifier }, { _id: new Types.ObjectId(_id) }]
+            }
+        )
         if (!user) {
             return res
                 .status(404)
@@ -43,16 +49,19 @@ export const login = async (req, res) => {
                 )
         }
         const token = createToken({ _id: user._id, email: user.email });
+        const fullUser = await getUser(identifier);
         return res
             .cookie(process.env.TokenName, token, CookieOptions)
             .json(
-                new ApiResponse('login successfully', { token, user })
+                new ApiResponse('login successfully', { token, user:fullUser })
             )
     } catch (error) {
+        console.log(error);
+
         return res
             .status(500)
             .json(
-                new ApiError('Server Error', error)
+                new ApiError('Server Error', { error }, false, 500)
             )
     }
 
