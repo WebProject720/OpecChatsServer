2febaddaaf87bcbbf7e05173bc490c595aab1c53
WebProject720 <rameshmalisundesha720@gmail.com>
2024-10-12 23:28:26 +0530

    Cookies Updates

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,24 +5,30 @@ import { verifyToken } from '../../src/utils/verifyToken.js';
 
 export const auth = async (req, res, next) => {
     try {
-        const cookie = req?.cookies[process.env.TokenName]||req?.cookies[process.env.GuestTokenName];
-        console.log(cookie,req?.cookies);
-        
-        
-        if (!cookie) {
+        const { user } = req.body || false;
+        const { guest } = req.body || false;
+
+        let cookie;
+        if (user) {
+            cookie = req?.cookies[process.env.TokenName];
+        } else if (guest) {
+            cookie = req?.cookies[process.env.GuestTokenName];
+        } else {
             return res.status(404).json(
                 new ApiError('Session not found')
             )
         }
+
+
         const user = verifyToken(cookie);
-        
-        
+
+
         if (!user) {
             return res.status(404).json(
                 new ApiError('Invalid Session ID')
             )
         }
-        
+
         req.body._id = user?._id || false;
         next();
     } catch (error) {
