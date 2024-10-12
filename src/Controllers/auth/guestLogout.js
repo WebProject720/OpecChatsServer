9d08ba62291dc6311d2e8c22b26e48b4c94040d3
WebProject720 <rameshmalisundesha720@gmail.com>
2024-10-13 00:42:13 +0530

    APIs Logouts

--- src/Controllers/auth/guestLogout.js
+++ src/Controllers/auth/guestLogout.js
@@ -6,11 +6,11 @@ export const GuestLogout = async (req, res) => {
     try {
         
         const { _id } = req.body;
-        if (!_id) {
-            return res.status(400).json(
-                new ApiError('Session not found')
-            )
-        }
+        // if (!_id) {
+        //     return res.status(400).json(
+        //         new ApiError('Session not found')
+        //     )
+        // }
         return res
             .status(200)
             .clearCookie(process.env.GuestTokenName)
