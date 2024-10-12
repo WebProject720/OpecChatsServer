9064024b78d4124eb79277bba360189da706d145
WebProject720 <rameshmalisundesha720@gmail.com>
2024-10-12 23:59:02 +0530

    revert cookies updates

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,31 +5,23 @@ import { verifyToken } from '../../src/utils/verifyToken.js';
 
 export const auth = async (req, res, next) => {
     try {
-        const { user } = req.body || false;
-        const { guest } = req.body || false;
+        const cookie = req?.cookies[process.env.TokenName] || req?.cookies[process.env.GuestTokenName];
 
-        let cookie;
-        if (user) {
-            cookie = req?.cookies[process.env.TokenName];
-        } else if (guest) {
-            cookie = req?.cookies[process.env.GuestTokenName];
-        } else {
+        if (!cookie) {
             return res.status(404).json(
                 new ApiError('Session not found')
             )
         }
+        const user = verifyToken(cookie);
 
 
-        const userLogged = verifyToken(cookie);
-
-
-        if (!userLogged) {
+        if (!user) {
             return res.status(404).json(
                 new ApiError('Invalid Session ID')
             )
         }
 
-        req.body._id = userLogged?._id || false;
+        req.body._id = user?._id || false;
         next();
     } catch (error) {
         return res.status(500).json(
